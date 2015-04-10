76ddc5fa8a94aab6e2b92f9bda8e283abd6655ca
mauriciodev <mauricio.dev@gmail.com>
2015-04-09 22:36:19 -0300

    Something went wrong with this git.

--- Qt/tokenFactory.py
+++ Qt/tokenFactory.py
@@ -1,25 +1,37 @@
 from PyQt4 import QtGui, QtSvg, QtCore
-import os
+import os,sys
 
+if getattr(sys, 'frozen', False):
+    # we are running in a |PyInstaller| bundle
+    basedir = sys._MEIPASS
+else:
+    # we are running in a normal Python environment
+    basedir = os.path.dirname(__file__)
 
 class TokenFactory(QtCore.QObject):
     def __init__(self,scene=None):
         self.scene=scene
-        dirname, filename = os.path.split(os.path.abspath(__file__))
-        self.tokenRenderer=QtSvg.QSvgRenderer(os.path.join(os.path.split(dirname)[0],'images','tokens.svg' ))
+        self.tokenRenderer=QtSvg.QSvgRenderer(os.path.join(os.path.split(basedir)[0],'images','tokens.svg' ))
+        self.tokenCount=0
     def newToken(self,parent,type,x=-20,y=0):
         if self.tokenRenderer.elementExists(type):
-            token=QtSvg.QGraphicsSvgItem(parent)
-            print token.boundingRect()
-            #token.boundingRect().setWidth(20)
-            #token.boundingRect().setHeight(20)
+            token=Token(parent,self.tokenCount)
+            self.tokenCount+=1
             token.setScale(0.3)
             token.setSharedRenderer(self.tokenRenderer)
             token.setElementId(type)
             token.setZValue(100)
-            token.setPos(-20,0)
+            token.setPos(x,y)
             return token
         else:
             print "Invalid token."
             return None
 
+class Token(QtSvg.QGraphicsSvgItem):
+    def __init__(self,parent=None,tokenId=0):
+        super(Token,self).__init__(parent)
+        self.tokenId=tokenId
+    
+    def getTokenType(self):
+        return self.elementId()
+    
